f32871ccb8058f6f4594deaf8bbd689983a5c7c2
Keshavsomani999 <95158743+Keshavsomani999@users.noreply.github.com>
2023-02-20 19:19:55 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./App.css";
+import Header from"./component/layout/Header/Header.js"
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import WebFont from "webfontloader";
+import Footer from"./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
+import ProductDetails from "./component/Product/ProductDetails.js"
+import Products from "./component/Product/Products.js"
+import Search from "./component/Product/Search.js"
+
+
+function App() {
+  React.useEffect(()=>{
+
+    WebFont.load({
+      google:{
+        families:["Roboto","Droid Sans","Chilanka"]
+      }
+    })
+  
+  },[])
+  return (
+    <Router>
+      <Header />
+      <Routes>
+
+      <Route extact path='/' element={<Home />} />
+      <Route extact path='/product/:id' element={<ProductDetails />} />
+      <Route extact path='/products' element={<Products />} />
+      <Route path='/products/:keyword' element={<Products />} />
+      <Route extact path='/search' element={<Search />} />
+
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
